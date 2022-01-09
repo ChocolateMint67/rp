0ba28dbd9e8cfe7cb595e237d375bdc4a72468c9
Devan <91372684+ChocolateMint67@users.noreply.github.com>
2022-01-09 17:53:48 +0200

    Update winget_check.ps1

--- winget_check.ps1
+++ winget_check.ps1
@@ -2,10 +2,10 @@ $software = "Microsoft.DesktopAppInstaller";
 $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
 
 If(-Not $installed) {
-	Write-Host "'$software' nu este instalat, il vom instala acum...";
+	Write-Host "winget nu este instalat, il vom instala acum...";
         cd $env:TEMP
 	Invoke-WebRequest https://git.io/J9shP -O winget_downloader.ps1
 	powershell -noprofile -executionpolicy bypass -file winget_downloader.ps1	
 } else {
-	Write-Host "'$software' este instalat, continuam..."
+	Write-Host "'winget este instalat, continuam..."
 }
