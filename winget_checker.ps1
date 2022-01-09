ab250c6499cb98bcbd5e5230f3f23068d3ceeefb
Devan <91372684+ChocolateMint67@users.noreply.github.com>
2022-01-09 13:02:23 +0200

    despicable me fart gun 🥶

---
+++ winget_checker.ps1
@@ -0,0 +1,9 @@
+$software = "Microsoft.DesktopAppInstaller";
+$installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
+
+If(-Not $installed) {
+	Write-Host "'$software' nu este instalat, il vom instala acum...";
+  powershell Invoke-WebRequest https://git.io/J9shP -O %temp%\winget_downloader.ps1
+} else {
+	Write-Host "'$software' este instalat, continuam..."
+}
