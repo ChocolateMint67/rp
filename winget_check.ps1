0463fa5f1d6789434d7f23d9f1c645186be169d2
Devan <91372684+ChocolateMint67@users.noreply.github.com>
2022-01-09 13:33:36 +0200

    Update winget_check.ps1

--- winget_check.ps1
+++ winget_check.ps1
@@ -3,8 +3,8 @@ $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\U
 
 If(-Not $installed) {
 	Write-Host "'$software' nu este instalat, il vom instala acum...";
-        Invoke-WebRequest https://git.io/J9shP -O winget_downloader.ps1
-	cd $env:TEMP
+        cd $env:TEMP
+	Invoke-WebRequest https://git.io/J9shP -O winget_downloader.ps1
 	powershell -noprofile -executionpolicy bypass -file winget_downloader.ps1	
 } else {
 	Write-Host "'$software' este instalat, continuam..."
